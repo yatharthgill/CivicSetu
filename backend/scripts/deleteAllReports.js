afd7655cfdd2713a8b380232e31c7f44715d6030
priyanshu <priyanshu88011@gmail.com>
2026-04-24 18:08:08 +0530

    fix delete allreport and all user and try to make frontend responcive

--- backend/scripts/deleteAllReports.js
+++ backend/scripts/deleteAllReports.js
@@ -1,7 +1,12 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, '../.env') });
 
 const cleanReports = async () => {
   try {
